ece2230daa4b3368dbe7c9bcd367ddd702414b93
ShawnIsADev <122501853+ShawnIsADev@users.noreply.github.com>
2023-03-12 07:57:55 -0400

    Updated index.js
    added an onclick animation for the hamburger menu using GSAP

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// GSAP animated hamburger menu - no content
 const bar = document.querySelectorAll(".bar")
 const h1 = document.getElementById("h1")
 const cube = document.querySelector(".cube")
 const shadow = document.querySelector(".shadow")
+const entireHamburgerMenu = document.querySelector(".toggle-hamburger-menu")
 
+entireHamburgerMenu.addEventListener('click', animate)
 
+function animate(){
+    gsap.from(bar, {duration: 2, x: () => Math.random() * 400 - 200, rotate: -360, stagger: 0.25})
+}
 
 gsap.from(bar, {duration: 2, x: () => Math.random() * 400 - 200, rotate: 360, stagger: 0.25})
 gsap.from(h1, {duration: 5, y: () => Math.random() * 400 - 200, rotate: 360, stagger: 0.25})
 gsap.from(shadow, {duration: 2, y: () => Math.random() * 400 - 200, rotate: 360})
-
