9272fe80a454ce083e8232f500ba2081d1500353
ShawnIsADev <122501853+ShawnIsADev@users.noreply.github.com>
2023-03-12 06:58:10 -0400

    Updated index.js
    added an animation for the shadow using Green Sock animation. Will remove if users don't enjoy it, or if it creates extra un-needed space on mobile.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const bar = document.querySelectorAll(".bar")
 const h1 = document.getElementById("h1")
 const cube = document.querySelector(".cube")
+const shadow = document.querySelector(".shadow")
+
+
+
 gsap.from(bar, {duration: 2, x: () => Math.random() * 400 - 200, rotate: 360, stagger: 0.25})
 gsap.from(h1, {duration: 5, y: () => Math.random() * 400 - 200, rotate: 360, stagger: 0.25})
+gsap.from(shadow, {duration: 2, y: () => Math.random() * 400 - 200, rotate: 360})
 
