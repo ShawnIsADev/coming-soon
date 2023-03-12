d279c9c2c01f71907c4a2efccd999176f19b0ca9
ShawnIsADev <122501853+ShawnIsADev@users.noreply.github.com>
2023-03-12 08:26:17 -0400

    updated index.js
    Utilized more capabilities of GSAP by making onclick function for all sides of the 3D cube to go in a 360 or -360 direction.

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const bar = document.querySelectorAll(".bar")
 const h1 = document.getElementById("h1")
 const cube = document.querySelector(".cube")
+const topOfCube = document.querySelector(".top")
+const frontOfCube = document.querySelector(".front")
+const backOfCube = document.querySelector(".back")
+const rightOfCube = document.querySelector(".right")
+const bottomOfCube = document.querySelector(".bottom")
+const leftOfCube = document.querySelector(".left")
+
 const shadow = document.querySelector(".shadow")
 const entireHamburgerMenu = document.querySelector(".toggle-hamburger-menu")
 
 entireHamburgerMenu.addEventListener('click', animate)
+cube.addEventListener("click", animatedOnClickCube)
+
 
 function animate(){
     gsap.from(bar, {duration: 2, x: () => Math.random() * 400 - 200, rotate: -360, stagger: 0.25})
+
 }
+
+function animatedOnClickCube(){
+    gsap.from(topOfCube, {duration: 3, x: () => Math.random() * 400 - 200, rotate: -360})
+    gsap.from(frontOfCube, {duration: 4, x: () => Math.random() * 400 - 200, rotate: 360})
+    gsap.from(backOfCube, {duration: 5, x: () => Math.random() * 400 - 200, rotate: -360})
+    gsap.from(rightOfCube, {duration: 6, x: () => Math.random() * 400 - 200, rotate: 360})
+    gsap.from(leftOfCube, {duration: 5, x: () => Math.random() * 400 - 200, rotate: -360})
+    gsap.from(bottomOfCube, {duration: 7, x: () => Math.random() * 400 - 200, rotate: 360})
+}
+
+
 
 gsap.from(bar, {duration: 2, x: () => Math.random() * 400 - 200, rotate: 360, stagger: 0.25})
 gsap.from(h1, {duration: 5, y: () => Math.random() * 400 - 200, rotate: 360, stagger: 0.25})
